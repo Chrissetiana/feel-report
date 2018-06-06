8486199f8a1105feffbe5cbccfacad2c806c7bdc
chrissetiana <chrissetiana@gmail.com>
2018-06-06 12:44:08 +0100

    Create layout

--- app/src/main/java/com/chrissetiana/feelreport/MainActivity.java
+++ app/src/main/java/com/chrissetiana/feelreport/MainActivity.java
@@ -1,9 +1,11 @@
 package com.chrissetiana.feelreport;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
